9f021027b3fac9321754220b80d151b8ea67fb0c
John Cadena <cadena.john.85@gmail.com>
2019-08-09 16:57:09 -0500

    add initial intro-to-testing-js project

--- code.js
+++ code.js
@@ -1,4 +1,16 @@
 // helloWorld function 
-function helloWorld() {
+/*function helloWorld() {
     return "Hello, World!";
+}*/
+
+const helloWorld = function() {
+    return "Hello, World!";
+}
+
+function sum(a, b) {
+    if (b === "sheep") {
+        return false
+    }  else {
+        return a + b;
+    }
 }
